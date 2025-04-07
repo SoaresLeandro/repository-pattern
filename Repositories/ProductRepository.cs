75814e1552f69c8a0ceff5276f24405bb7b78af2
Leandro <lss.91@live.com>
2025-04-07 14:40:45 -0400

    crud

---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryPattern.Data;
+using RepositoryPattern.Entities;
+
+namespace RepositoryPattern.Repositories
+{
+    public class ProductRepository(AppDbContext appDbContext)
+    {
+        public async Task<Product> CreateAsync(Product product, CancellationToken cancellationToken = default)
+        {
+            await appDbContext.Products.AddAsync(product);
+            await appDbContext.SaveChangesAsync();
+
+            return product;
+        }
+
+        public async Task<Product> UpdateAsync(Product product, CancellationToken cancellationToken = default)
+        {
+            appDbContext.Products.Update(product);
+            await appDbContext.SaveChangesAsync();
+
+            return product;
+        }
+
+        public async Task<Product> DeleteAsync(Product product, CancellationToken cancellationToken = default)
+        {
+            appDbContext.Remove(product);
+            await appDbContext.SaveChangesAsync();
+
+            return product;
+        }
+
+        public async Task<Product?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+        {
+            return await appDbContext.Products.FirstOrDefaultAsync(x => x.Id.Equals(id));
+        }
+
+        public async Task<List<Product>> GetAllAsync(int skip = 0, int take = 10)
+        {
+            return await appDbContext.Products
+                .AsNoTracking()
+                .Skip(skip)
+                .Take(take)
+                .ToListAsync();
+        }
+    }
+}
